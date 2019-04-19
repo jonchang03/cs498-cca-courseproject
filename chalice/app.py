69d32cd5d5bd3afc0142bb4c68a8e3d634914d6b
dk30694 <36805051+dk30694@users.noreply.github.com>
2019-04-18 23:33:14 -0400

    Changes to call S3 from chalice
    Added changes to make a direct call to s3 bucket for model/test_X.h5

--- chalice/app.py
+++ chalice/app.py
@@ -9,17 +9,25 @@ import requests
 import boto3
 import h5py
 import json
+from io import BytesIO
 
 app = Chalice(app_name='chalice')
 app.debug = True
 
-
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('test-bucket')
 file_name = 's3://malariaimages498/cell_images/Parasitized/C100P61ThinF_IMG_20150918_144104_cell_168.png'
 endpoint_name = 'sagemaker-tensorflow-2019-04-13-16-22-06-941'
 #s3://sagemaker-us-east-1-554240446913/model/model.tar.gz
 #s3://sagemaker-us-east-1-554240446913/model/test_X.h5
 
-h5f = h5py.File('s3://sagemaker-us-east-1-554240446913/model/test_X.h5','r')
+#h5f = h5py.File('s3://sagemaker-us-east-1-554240446913/model/test_X.h5','r')
+s3client = boto3.client('s3') #low-level functional API
+resource = boto3.resource('s3') #high-level object-oriented API
+my_bucket = resource.Bucket('sagemaker-us-east-1-554240446913') #subsitute this for your s3 bucket name.
+obj = s3client.get_object(Bucket='sagemaker-us-east-1-554240446913', Key='model/test_X.h5')
+h5f = h5py.File(BytesIO(obj['Body'].read()))
+
 test_X = h5f['test_X'][:]
 h5f.close()
 
